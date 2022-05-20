69c30356e8b9e98fdf9949f3cb18513acaa270b5
ricky <herickyhe@gmail.com>
2022-05-20 16:05:46 +0800

    7-3-2 7-3-2 註冊功能(3) 使用 bcryptjs 加/解密敏感性資訊

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 
 ////////////////////////////////////////////////////////////////
@@ -24,37 +25,29 @@ const postLogin = (req, res) => {
     const { email, password } = req.body;
     User.findOne({ where: { email } })
         .then((user) => {
+            console.log('user', user);
             if (!user) {
                 req.flash('errorMessage', '錯誤的 Email 或 Password。');
                 return res.redirect('/login');
             }
-            if (user.password === password) {
-                console.log('login: 成功');
-                req.session.isLogin = true;
-                return res.redirect('/')
-            }
-            req.flash('errorMessage', '錯誤的 Email 或 Password。');
-            res.redirect('/login');
-        })
-        .catch((err) => {
-            console.log('login error:', err);
-        });
-};
-
-
-const postSignup = (req, res) => {
-    const { displayName, email, password } = req.body;
-    User.findOne({ where: { email } })
-        .then((user) => {
-            if (user) {
-                req.flash('errorMessage', '此帳號已存在！請使用其他 Email。')
-                return res.redirect('/signup');
-            } else {
-                return User.create({ displayName, email, password });
-            }
-        })
-        .then((result) => {
-            res.redirect('/login');
+            bcryptjs
+                .compare(password, user.password)
+                .then((isMatch) => {
+                    console.log('isMatch', isMatch);
+                    if (isMatch) {
+                        req.session.user = user;
+                        req.session.isLogin = true;
+                        return req.session.save((err) => {
+                            console.log('postLogin - save session error: ', err);
+                            res.redirect('/');
+                        });
+                    }
+                    req.flash('errorMessage', '錯誤的 Email 或 Password。')
+                    res.redirect('/login');
+                })
+                .catch((err) => {
+                    return res.redirect('/login');
+                })
         })
         .catch((err) => {
             console.log('signup_error', err);
@@ -64,6 +57,31 @@ const postLogout = (req, res) => {
     req.session.destroy((err) => {
         res.redirect('/login')
     });
+}
+
+const postSignup = (req, res) => {
+    const { displayName, email, password } = req.body;
+    User.findOne({ where: { email } })
+        .then((user) => {
+            if (user) {
+                req.flash('errorMessage', '此帳號已存在！請使用其他 Email。')
+                return res.redirect('/signup');
+            } else {
+                return bcryptjs.hash(password, 12)
+                    .then((hashedPassword) => {
+                        return User.create({ displayName, email, password: hashedPassword });
+                    })
+                    .catch((err) => {
+                        console.log('create new user error: ', err);
+                    })
+            }
+        })
+        .then((result) => {
+            res.redirect('/login');
+        })
+        .catch((err) => {
+            console.log('signup_error', err);
+        });
 }
 
 module.exports = {
